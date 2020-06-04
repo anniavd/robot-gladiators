e563206d545bc5f0b3b5a704ff70ee59e36f8f3c
annia valdes <millerl.mb79@gmail.com>
2020-06-04 15:22:02 -0500

    add rondom fuction and update enemyhealthy and playerhealthy

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,15 +27,21 @@ var fight = function (enemyName) {
 
       // if yes (true), leave fight
       if (confirmSkip) {
+
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
+
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
+       
         console.log("playerMoney", playerMoney);
         break;
       }      
     }
-     // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      // remove enemy's health by subtracting the amount set in the playerAttack variable      
+      // generate random damage value based on player's attack power
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+      enemyHealth = Math.max(0, enemyHealth - damage);
+
       console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
      
       // check enemy's health
@@ -51,8 +57,11 @@ var fight = function (enemyName) {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
       }
 
-      // remove player's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      // remove player's health by subtracting the amount set in the enemyAttack variable    
+
+       var damage = randomNumber(enemyAttack - 3, enemyAttack);
+       playerHealth = Math.max(0, playerHealth - damage);
+
       console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");       
       
    }
@@ -74,8 +83,8 @@ var fight = function (enemyName) {
   
         var pickedEnemyName = enemyNames[i];
   
-        enemyHealth = 50;
-  
+        enemyHealth = randomNumber(40,60);
+        
         fight(pickedEnemyName);
 
            // if player is still alive and we're not at the last enemy in the array
@@ -176,6 +185,14 @@ var shop = function() {
     }
   
 };
+
+// function to generate a random numeric value
+
+  var randomNumber = function(min,max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+  
+    return value;
+  };
 
 // call to the fuction stargame
 startGame();
